5b7f0c75540ce3f21eaa647b98eb04c90541bc8c
Havoc Pennington <hp@pobox.com>
2012-05-09 19:09:50 -0400

    release 0.5.2

--- project/Build.scala
+++ project/Build.scala
@@ -22,7 +22,7 @@ object StartScriptBuild extends Build {
             // Versions and git tags should follow: http://semver.org/
             // except using -SNAPSHOT instead of without hyphen.
 
-            version := "0.5.1-SNAPSHOT",
+            version := "0.5.2",
             libraryDependencies <++= sbtVersion {
 		(version) =>
 		    Seq("org.scala-sbt" %% "io" % version % "provided",
