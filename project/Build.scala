195a1a8db5e4d59b39bcd9e88d2b02bafe9e6262
Mike Slinn <mslinn@micronauticsresearch.com>
2012-07-29 12:25:55 -0700

    Changed version to 0.5.2-mslinn

--- project/Build.scala
+++ project/Build.scala
@@ -1,7 +1,6 @@
 import sbt._
 
 import Keys._
-import Project.Initialize
 
 object StartScriptBuild extends Build {
     lazy val root =
@@ -13,24 +12,19 @@ object StartScriptBuild extends Build {
             name := "xsbt-start-script-plugin",
             scalacOptions := Seq("-unchecked", "-deprecation"),
 
-            // to release, bump major/minor/micro as appropriate,
-            // drop SNAPSHOT, tag and publish.
+            // to release, bump major/minor/micro as appropriate, drop SNAPSHOT, tag and publish.
             // add snapshot back so git master is a SNAPSHOT.
-            // when releasing a SNAPSHOT to the repo, bump the micro
-            // version at least.
+            // when releasing a SNAPSHOT to the repo, bump the micro version at least.
             // Also, change the version number in the README.md
             // Versions and git tags should follow: http://semver.org/
             // except using -SNAPSHOT instead of without hyphen.
 
-            version := "0.5.2-SNAPSHOT",
-            libraryDependencies <++= sbtVersion {
-		(version) =>
-		    Seq("org.scala-sbt" %% "io" % version % "provided",
-			"org.scala-sbt" %% "logging" % version % "provided",
-			"org.scala-sbt" %% "process" % version % "provided")
+            version := "0.5.2-mslinn",
+            libraryDependencies <++= sbtVersion { version =>
+              Seq("org.scala-sbt" %% "io" % version % "provided",
+                  "org.scala-sbt" %% "logging" % version % "provided",
+                  "org.scala-sbt" %% "process" % version % "provided")
             },
-
-            // publish stuff
             publishTo <<= (version) { v =>
                 import Classpaths._
                 Option(if (v endsWith "SNAPSHOT") typesafeSnapshots else typesafeResolver)
